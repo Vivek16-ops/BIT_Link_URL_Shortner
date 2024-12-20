17973ed7bed5d5337daeab656354888dfc438850
Vivek <rajv0719@gmail.com>
2024-12-20 10:20:21 +0530

    Designing Navbar and Home Section

--- app/page.js
+++ app/page.js
@@ -1,9 +1,26 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      Heyy I am main
+      <section className="grid grid-cols-2 h-[50vh] bg-purple-100">
+        <div className="flex flex-col justify-center items-center gap-3">
+          <p className={`text-5xl font-bold`}>
+            The best URL Shortner in the Market
+          </p>
+          <p className="text-2xl px-5 text-center">
+            We are the most straightforward URL Shortner in the world.Most of the url shortner will track you or ask you to give your details for login. We understand your needs and hence we have created this URL Shortner.
+          </p>
+          <div className='flex gap-3'>
+            <Link href={"/generate"}><button className="bg-purple-500 rounded-lg shadow-lg px-2 py-1 font-bold transform hover:scale-105 transition-transform duration-200 text-white"> Try Now </button></Link>
+            <Link href={"https://github.com/Vivek16-ops?tab=repositories"}><button className="bg-purple-500 rounded-lg shadow-lg px-2 py-1 font-bold transform hover:scale-105 transition-transform duration-200 text-white"> GitHub </button></Link>
+          </div>
+        </div>
+        <div className="flex justify-start relative">
+          <Image src={"/vector.jpg"} fill={true} alt="Banner Image" className="mix-blend-darken" />
+        </div>
+      </section>
     </main>
   );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,22 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <nav className='h-10 bg-purple-700 text-white flex justify-between items-center px-3'>
-        <div className="logo font-bold text-lg">
-            BITLink
-        </div>
-       <ul className='flex justify-center items-center gap-4'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Shortner</li>
-        <li>Contact Us</li>
-       </ul>
+    <nav className='h-20 bg-purple-700 text-white flex justify-between items-center px-3'>
+      <div className="logo font-bold text-2xl">
+        BITLink
+      </div>
+      <ul className='flex justify-center items-center gap-4 text-xl'>
+        <Link className='hover:font-semibold' href={"/"}><li>Home</li></Link>
+        <Link className='hover:font-semibold' href={"/about"}><li>About</li></Link>
+        <Link className='hover:font-semibold' href={"/generate"}><li>Shortner</li></Link>
+        <Link className='hover:font-semibold' href={"/contact"}><li>Contact Us</li></Link>
+        <li className='flex gap-3'>
+          <Link href={"/generate"}><button className="bg-purple-500 rounded-lg shadow-lg px-2 py-1 font-bold transform hover:scale-105 transition-transform duration-200"> Try Now </button></Link>
+          <Link href={"https://github.com/Vivek16-ops?tab=repositories"}><button className="bg-purple-500 rounded-lg shadow-lg px-2 py-1 font-bold transform hover:scale-105 transition-transform duration-200"> GitHub </button></Link>
+        </li>
+      </ul>
     </nav>
   )
 }
